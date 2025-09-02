5e1a53f78d298180a6e98bf4a3a766fb24e693e6
farstar-galaxy <610237307@qq.com>
2025-09-02 10:08:15 +0800

    年均数据

---
+++ aver.py
@@ -0,0 +1,26 @@
+from data import DATA
+city_yearly_count={}
+city_yearly_sum={}
+
+for index, row in DATA.iterrows():
+    city = row['city_name']
+    value = row['value']
+    year = row['year']
+
+    if city not in city_yearly_sum:
+        city_yearly_sum[city] = {}
+        city_yearly_count[city] = {}
+
+    if year not in city_yearly_sum[city]:
+        city_yearly_sum[city][year] = 0
+        city_yearly_count[city][year] = 0
+
+    city_yearly_count[city][year] += 1
+    city_yearly_sum[city][year] += value
+
+city_yearly_avg = {}
+for city in city_yearly_sum:
+    city_yearly_avg[city] ={}
+    for year in city_yearly_sum[city]:
+        city_yearly_avg[city][year] = city_yearly_sum[city][year] / city_yearly_count[city][year]
+print("每个城市每年的平均值：", city_yearly_avg)
